fde4c3f34884a8bf6c199314ecb1bcc62e6766a0
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2022-10-20 13:27:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function nineEnd() {
 $(document).ready(function () {
     var ctx = document.getElementById("canvas").getContext("2d");
     var radarChartData = {
-        labels: ["kiểu hoàn hảo", "kiểu trợ giúp", "kiểu thành tích", "kiểu bản ngã", "kiểu hợp lý", "kiểu nghi ngờ", "kiểu tích cực", "kiểu lãnh đạo", "kiểu ôn hòa"],
+        labels: ["完美型", "助人型", "成就型", "自我型", "理智型", "疑惑型", "活跃型", "领袖型", "和平型"],
         datasets: [
             {
                 label: "Nine",
